0de9ff957523b807ebda07c71349445669eee3f1
Harvey <103399683+hr17045@users.noreply.github.com>
2023-04-04 14:16:41 +1200

    4th April
    encountered an error when connecting my database to my code, fixed using research and testing and problem was simple name errors for my labels in login page (email)

--- app.py
+++ app.py
@@ -3,40 +3,42 @@ from flask_sqlalchemy import SQLAlchemy
 import calendar, datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Harvey/Desktop/SCHOL2023/instance/user.db'
+
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     department = db.Column(db.String(120), nullable=False)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         department = request.form['department']
         
         # Query the database for the submitted username and password
-        user = User.query.filter_by(username=username, password=password).first()
+        user = User.query.filter_by(email=email, password=password, department=department).first()
+
         if user:
             return 'Login successful!'
         else:
-            return 'Invalid username or password'
+            return 'Invalid email or password'
     
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         department = request.form['department']
 
         # Create a new user object and add it to the database
-        new_user = User(username=username, password=password, department=department)
+        new_user = User(email=email, password=password, department=department)
         db.session.add(new_user)
         db.session.commit()
 
