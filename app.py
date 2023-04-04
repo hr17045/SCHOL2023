cc5b15c3f53150f99b5677b81a2aede2bdef5312
Harvey <103399683+hr17045@users.noreply.github.com>
2023-04-04 12:35:20 +1200

    4th April
    code for routes to login, signup page and main calendar page. Connected it to my database user.db. html and java and css for login and signup page

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import calendar, datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    department = db.Column(db.String(120), nullable=False)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        department = request.form['department']
+        
+        # Query the database for the submitted username and password
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            return 'Login successful!'
+        else:
+            return 'Invalid username or password'
+    
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        department = request.form['department']
+
+        # Create a new user object and add it to the database
+        new_user = User(username=username, password=password, department=department)
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Display a success message
+        return 'Sign up successful!'
+
+    return render_template('signup.html')
+
+@app.route("/")
+def index():
+    # Get the current year and month
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+
+    # Create a calendar object
+    cal = calendar.monthcalendar(year, month)
+
+    # Render the calendar template
+    return render_template("calendar.html", cal=cal, year=year, month=month)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- calvert.py
+++
@@ -1,3 +0,0 @@
-# This is a test to link my github with Github Desktop asnd VS Code!
-
-print("success")
