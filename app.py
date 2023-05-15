9baed5c55558a28d6428713a47f218e351b1dbf4
Harvey <103399683+hr17045@users.noreply.github.com>
2023-05-16 11:47:43 +1200

    Successfully created addtask and database for tasks
    The HTML template includes a hidden modal (<div id="addTaskModal" style="display: none;">) that contains the form for adding a task.
    
    The "Add Task" button is added as a custom button in the FullCalendar initialization code:
    customButtons: {
        addTaskButton: {
            text: 'Add Task',
            click: function() {
                document.getElementById('addTaskModal').style.display = 'block';
            }
        }
    },
    When the "Add Task" button is clicked, it sets the display property of the modal to "block", making it visible to the user.
    
    The form in the modal has an action attribute set to /add_task, which corresponds to the Flask route for adding tasks.
    
    When the user submits the form, the add_task route in Flask is triggered:
    @app.route('/add_task', methods=['POST'])
    def add_task():
        title = request.form['title']
        start_date = datetime.strptime(request.form['start_date'], '%Y-%m-%d')
        end_date = datetime.strptime(request.form['end_date'], '%Y-%m-%d')
        new_task = Task(title=title, start_date=start_date, end_date=end_date)
        db.session.add(new_task)
        db.session.commit()
        return redirect('/')
    The route extracts the task details from the form data, creates a new Task object, adds it to the database, and commits the changes. Finally, it redirects back to the calendar page.
    
    The calendar route in Flask retrieves all tasks from the database and formats them as event objects to be displayed on the calendar.
    In summary, the code allows users to add tasks with a name, start date, and end date through the modal form. The tasks are saved to the database and displayed on the calendar when the page is refreshed.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-import calendar, datetime
-import hashlib
+from datetime import datetime
 
 app = Flask(__name__, static_url_path='/static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Harvey/Desktop/SCHOL2023/instance/user.db'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Harvey/Desktop/SCHOL2023/instance/user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Harvey/Desktop/SCHOL2023/instance/task.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -14,65 +14,35 @@ class User(db.Model):
     password = db.Column(db.String(120), nullable=False)
     department = db.Column(db.String(120), nullable=False)
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-events = [
-    {
-        'todo' : 'Tutorial for Alex',
-        'date' : '2023-05-08', 
-    },
-    {
-        'todo' : 'Eat Tuckshop',
-        'date' : '2023-05-09',
-    }
-]
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
 
 @app.route("/")
 def calendar():
-    return render_template('calendar.html', events = events)
+    tasks = Task.query.all()
+    events = []
+    for task in tasks:
+        event = {
+            'title': task.title,  # Include the task title in the event object
+            'start': task.start_date.strftime('%Y-%m-%d'),
+            'end': task.end_date.strftime('%Y-%m-%d')
+        }
+        events.append(event)
+    return render_template('calendar.html', events=events)
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        #hashing passwords
-        password_h = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        
-        # Query the database for the submitted username and password
-        user = User.query.filter_by(email=email, password=password_h).first()
-
-        if user:
-            return redirect('/')
-        else:
-            return 'Invalid email or password'
-    
-    return render_template('login.html')
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        department = request.form['department']
-       #hashing passwords
-        password_h = hashlib.sha256(password.encode('utf-8')).hexdigest()
-
-        # Create a new user object and add it to the database
-        new_user = User(email=email, password=password_h, department=department)
-        db.session.add(new_user)
-        db.session.commit()
-
-        # Display a success message
-        return redirect('/') 
-
-    return render_template('signup.html')
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    title = request.form['title']
+    start_date = datetime.strptime(request.form['start_date'], '%Y-%m-%d')
+    end_date = datetime.strptime(request.form['end_date'], '%Y-%m-%d')
+    new_task = Task(title=title, start_date=start_date, end_date=end_date)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
