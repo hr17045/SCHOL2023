3e7f14c1c1600b852650671b7e0e1eb58401a36b
Harvey <103399683+hr17045@users.noreply.github.com>
2023-07-21 10:21:04 +1200

    21st July
    code updated to have a login button on the calendar page. recognisable school emblem added to the page. code has been commented properly.

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import calendar
 from datetime import datetime
 import hashlib
 
+# Create the Flask app and set the secret key for session management
 app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace 'your_secret_key_here' with a secret key
+
+# Configure the database URI for user and task data
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Harvey/Desktop/SCHOL2023/instance/user.db'
 app.config['SQLALCHEMY_BINDS'] = {
     'task': 'sqlite:///C:/Users/Harvey/Desktop/SCHOL2023/instance/task.db'
 }
+
+# Create the SQLAlchemy instance for database management
 db = SQLAlchemy(app)
 
+# Define the User model to represent user data in the database
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     department = db.Column(db.String(120), nullable=False)
 
+# Define the Task model to represent task data in the separate 'task' database
 class Task(db.Model):
     __bind_key__ = 'task'  # Specify the database binding
     id = db.Column(db.Integer, primary_key=True)
@@ -24,71 +32,90 @@ class Task(db.Model):
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime, nullable=False)
 
-
-
-
-
-
+# Route for the main calendar page
 @app.route("/")
 def calendar():
+    # Retrieve all tasks from the 'task' database
     tasks = Task.query.all()
     events = []
     for task in tasks:
+        # Convert each task to a format suitable for FullCalendar
         event = {
-            'title': task.title,  # Include the task title in the event object
+            'title': task.title,
             'start': task.start_date.strftime('%Y-%m-%d'),
             'end': task.end_date.strftime('%Y-%m-%d')
         }
         events.append(event)
     return render_template('calendar.html', events=events)
 
+# Route for adding a new task via a form submission
 @app.route('/add_task', methods=['POST'])
 def add_task():
+    # Retrieve task details from the submitted form data
     title = request.form['title']
     start_date = datetime.strptime(request.form['start_date'], '%Y-%m-%d')
     end_date = datetime.strptime(request.form['end_date'], '%Y-%m-%d')
+
+    # Create a new task instance with the provided details
     new_task = Task(title=title, start_date=start_date, end_date=end_date)
+
+    # Add the new task to the 'task' database and commit the changes
     db.session.add(new_task)
     db.session.commit()
+
+    # Redirect the user back to the main calendar page
     return redirect('/')
 
-
+# Route for user login and authentication
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        # Retrieve user credentials from the submitted form data
         email = request.form['email']
         password = request.form['password']
-        #hashing passwords
         password_h = hashlib.sha256(password.encode('utf-8')).hexdigest()
         
-        # Query the database for the submitted username and password
+        # Check if a user with the provided email and password exists in the 'user' database
         user = User.query.filter_by(email=email, password=password_h).first()
 
         if user:
-            return redirect('/')
+            # Store the user ID in the session to manage the user's login state
+            session['user_id'] = user.id
+            return redirect('/')  # Redirect the user to the main calendar page after successful login
         else:
-            return 'Invalid email or password'
+            return 'Invalid email or password'  # Display an error message for invalid credentials
     
-    return render_template('login.html')
+    return render_template('login.html')  # Render the login page template for GET requests
 
+# Route for user signup and adding a new user to the 'user' database
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        # Retrieve user details from the submitted form data
         email = request.form['email']
         password = request.form['password']
         department = request.form['department']
-       #hashing passwords
         password_h = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
-        # Create a new user object and add it to the database
+        # Create a new user instance with the provided details
         new_user = User(email=email, password=password_h, department=department)
+
+        # Add the new user to the 'user' database and commit the changes
         db.session.add(new_user)
         db.session.commit()
 
-        # Display a success message
-        return redirect('/') 
+        return redirect('/')  # Redirect the user to the main calendar page after successful signup
 
-    return render_template('signup.html')
+    return render_template('signup.html')  # Render the signup page template for GET requests
 
+# Route for user logout and clearing the session data
+@app.route('/logout')
+def logout():
+    # Clear the session data to log out the user
+    session.pop('user_id', None)
+
+    return redirect('/login')  # Redirect the user back to the login page
+
+# Start the Flask application in debug mode if this script is run as the main module
 if __name__ == "__main__":
     app.run(debug=True)
